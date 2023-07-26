6a809ceb3221cc96f2190bb939f2ca9869469e50
higashiyama-shunya <higashiyama.shunya@oysterworld.jp>
2023-07-26 14:51:52 +0900

    確認のためのstatic
    staticで動くかどうか確認するためにやった。
    本当はスクリプトを2つに分けてやるようにするので、それをやっていく。

--- Schroll.cs
+++ Schroll.cs
@@ -14,7 +14,12 @@ public class Schroll : MonoBehaviour, IDynamicScrollViewItem
         };
 
 
-    public List<ChatListResult> list;
+    public static List<ChatListResult> list = new List<ChatListResult>()
+    {
+        new ChatListResult(){id=1,chat_room_name="room1",owner_id=1},
+        new ChatListResult(){id=2,chat_room_name="room2",owner_id=2},
+        new ChatListResult(){id=3,chat_room_name="room3",owner_id=3},
+    };
 
     [SerializeField]
     public DynamicScrollView scrollView;
@@ -26,11 +31,13 @@ public class Schroll : MonoBehaviour, IDynamicScrollViewItem
 
     private void Start()
     {
+        /*
         for (int i = 1; i < 4; i++)
         {
             list.Add(new ChatListResult { id = i, chat_room_name = "room" + i, owner_id = i });
             //Debug.Log(list[i - 1].chat_room_name);
         }
+        */
         //Debug.Log("listcount:" + list.Count + "\ntotalItemCount" + scrollView.totalItemCount);
         scrollView.totalItemCount = list.Count;
         Debug.Log("listcount:" + list.Count + "\ntotalItemCount" + scrollView.totalItemCount);
