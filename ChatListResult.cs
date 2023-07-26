921cc520e4445136e8e71d6b2538361feeaa513e
higashiyama-shunya <higashiyama.shunya@oysterworld.jp>
2023-07-26 14:37:34 +0900

    test
    ダミーデータを表示したが、自分のリストが上手く表示できていない。この後staticにテストで帰る

---
+++ ChatListResult.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class ChatListResult
+{
+    public int id;
+    public string chat_room_name;
+    public string created_at;
+    public string updated_at;
+    public int owner_id;
+}
---
+++ Schroll.cs
@@ -0,0 +1,55 @@
+using Mosframe;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
+
+public class Schroll : MonoBehaviour, IDynamicScrollViewItem
+{
+    private readonly Color[] colors = new Color[] {
+            Color.cyan,
+            Color.green,
+        };
+
+
+    public List<ChatListResult> list;
+
+    [SerializeField]
+    public DynamicScrollView scrollView;
+    [SerializeField]
+    public Text title;
+    [SerializeField]
+    public Image background;
+
+
+    private void Start()
+    {
+        for (int i = 1; i < 4; i++)
+        {
+            list.Add(new ChatListResult { id = i, chat_room_name = "room" + i, owner_id = i });
+            //Debug.Log(list[i - 1].chat_room_name);
+        }
+        //Debug.Log("listcount:" + list.Count + "\ntotalItemCount" + scrollView.totalItemCount);
+        scrollView.totalItemCount = list.Count;
+        Debug.Log("listcount:" + list.Count + "\ntotalItemCount" + scrollView.totalItemCount);
+
+    }
+
+    public void onUpdateItem(int index)
+    {
+        Debug.Log("onUpdateItemäJén:" + index);
+        Debug.Log("listcount:" + list.Count + "\n index:" + index);
+        if (list.Count > index)
+        {
+            var data = list[index];
+
+            background.color = this.colors[Mathf.Abs(index) % colors.Length];
+
+            title.text = string.Format("{0}:{1}", data.id, data.chat_room_name);
+        }
+        Debug.Log("onUpdateItemèIóπ:" + index);
+    }
+
+}
